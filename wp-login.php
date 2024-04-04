1e3c5a574ebb2cc38223935bd806969188547556
Felix Andrews <felix@nfrac.org>
2024-04-05 10:03:33 +1100

    dump

---
+++ wp-login.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+	<html lang="en-AU">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; Stand up to the Grownups &#8212; WordPress</title>
+	<meta name='robots' content='max-image-preview:large, noindex, noarchive' />
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' id='dashicons-css'  href='https://standuptothegrownups.net/wp-includes/css/dashicons.min.css?ver=5.8.9' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://standuptothegrownups.net/wp-includes/css/buttons.min.css?ver=5.8.9' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://standuptothegrownups.net/wp-admin/css/forms.min.css?ver=5.8.9' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://standuptothegrownups.net/wp-admin/css/l10n.min.css?ver=5.8.9' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://standuptothegrownups.net/wp-admin/css/login.min.css?ver=5.8.9' media='all' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="https://standuptothegrownups.net/wp-content/uploads/2020/01/cropped-writing_header-2-32x32.jpg" sizes="32x32" />
+<link rel="icon" href="https://standuptothegrownups.net/wp-content/uploads/2020/01/cropped-writing_header-2-192x192.jpg" sizes="192x192" />
+<link rel="apple-touch-icon" href="https://standuptothegrownups.net/wp-content/uploads/2020/01/cropped-writing_header-2-180x180.jpg" />
+<meta name="msapplication-TileImage" content="https://standuptothegrownups.net/wp-content/uploads/2020/01/cropped-writing_header-2-270x270.jpg" />
+	</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-en-au">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="https://standuptothegrownups.net/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+									<input type="hidden" name="redirect_to" value="https://standuptothegrownups.net/wp-admin/" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
+
+					<p id="nav">
+								<a href="https://standuptothegrownups.net/wp-login.php?action=lostpassword">Lost your password?</a>
+			</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog">
+			<a href="https://standuptothegrownups.net/">&larr; Go to Stand up to the Grownups</a>		</p>
+			</div>
+	<script src='https://standuptothegrownups.net/wp-includes/js/jquery/jquery.min.js?ver=3.6.0' id='jquery-core-js'></script>
+<script src='https://standuptothegrownups.net/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2' id='jquery-migrate-js'></script>
+<script id='zxcvbn-async-js-extra'>
+var _zxcvbnSettings = {"src":"https:\/\/standuptothegrownups.net\/wp-includes\/js\/zxcvbn.min.js"};
+</script>
+<script src='https://standuptothegrownups.net/wp-includes/js/zxcvbn-async.min.js?ver=1.0' id='zxcvbn-async-js'></script>
+<script src='https://standuptothegrownups.net/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.7' id='regenerator-runtime-js'></script>
+<script src='https://standuptothegrownups.net/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0' id='wp-polyfill-js'></script>
+<script src='https://standuptothegrownups.net/wp-includes/js/dist/hooks.min.js?ver=a7edae857aab69d69fa10d5aef23a5de' id='wp-hooks-js'></script>
+<script src='https://standuptothegrownups.net/wp-includes/js/dist/i18n.min.js?ver=5f1269854226b4dd90450db411a12b79' id='wp-i18n-js'></script>
+<script id='wp-i18n-js-after'>
+wp.i18n.setLocaleData( { 'text direction\u0004ltr': [ 'ltr' ] } );
+</script>
+<script id='password-strength-meter-js-extra'>
+var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+</script>
+<script id='password-strength-meter-js-translations'>
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2024-02-23 01:50:34+0000","generator":"GlotPress\/4.0.0-beta.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"en_AU"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
+</script>
+<script src='https://standuptothegrownups.net/wp-admin/js/password-strength-meter.min.js?ver=5.8.9' id='password-strength-meter-js'></script>
+<script src='https://standuptothegrownups.net/wp-includes/js/underscore.min.js?ver=1.13.1' id='underscore-js'></script>
+<script id='wp-util-js-extra'>
+var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
+</script>
+<script src='https://standuptothegrownups.net/wp-includes/js/wp-util.min.js?ver=5.8.9' id='wp-util-js'></script>
+<script id='user-profile-js-extra'>
+var userProfileL10n = {"user_id":"0","nonce":"2d8534d470"};
+</script>
+<script id='user-profile-js-translations'>
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2024-02-23 01:50:34+0000","generator":"GlotPress\/4.0.0-beta.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"en_AU"},"Your new password has not been saved.":["Your new password has not been saved."],"Hide":["Hide"],"Show":["Show"],"Show password":["Show password"],"Confirm use of weak password":["Confirm use of weak password"],"Hide password":["Hide password"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
+</script>
+<script src='https://standuptothegrownups.net/wp-admin/js/user-profile.min.js?ver=5.8.9' id='user-profile-js'></script>
+	<script>
+	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
+	</script>
+		<div class="clear"></div>
+	</body>
+	</html>
+	
